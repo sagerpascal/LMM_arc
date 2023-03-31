24efd8aa90fa33cb9971989ca80d1526066e2d0c
sagerpascal <paes.sager@gmail.com>
2023-03-31 09:58:09 +0200

    Bugfix task 477d2879

--- simple_task_creator.py
+++ simple_task_creator.py
@@ -2,6 +2,7 @@
 # i.e. four vertical bars in a 9x9 grid, color bar according to their size (yellow, green, red, blue -> from small to big)
 
 import random
+import warnings
 from pathlib import Path
 
 import matplotlib
@@ -53,7 +54,10 @@ def create_single_task_477d2879(image=None):
     def remove_single_pixels(data):
         for i in range(0, data.shape[0]):
             for j in range(0, data.shape[1]):
-                if ((data[max(i-1, 0), j] != data[i, j] or i==0) and (data[min(i+1, data.shape[0]-1), j] != data[i, j] or i == data.shape[0]-1)) or ((data[i, max(j-1, 0)] != data[i, j] or j==0)  and (data[i, min(j+1, data.shape[1]-1)] != data[i, j] or j == data.shape[1]-1)):
+                if ((data[max(i - 1, 0), j] != data[i, j] or i == 0) and (
+                        data[min(i + 1, data.shape[0] - 1), j] != data[i, j] or i == data.shape[0] - 1)) or (
+                        (data[i, max(j - 1, 0)] != data[i, j] or j == 0) and (
+                        data[i, min(j + 1, data.shape[1] - 1)] != data[i, j] or j == data.shape[1] - 1)):
                     data[i, j] = 0
         return data
 
@@ -68,18 +72,29 @@ def create_single_task_477d2879(image=None):
         for i in range(0, data.shape[0]):
             for j in range(0, data.shape[1]):
                 if data[i, j] != 0:
-                    if data[max(i-1, 0), j] != data[i, j] or data[min(i+1, data.shape[0]-1), j] != data[i, j] or data[i, max(j-1, 0)] != data[i, j] or data[i, min(j+1, data.shape[0]-1)] != data[i, j] or data[max(i-1, 0), max(j-1, 0)] != data[i, j] or data[max(i-1, 0), min(j+1, data.shape[0]-1)] != data[i, j] or data[min(i+1, data.shape[0]-1), max(j-1, 0)] != data[i, j] or data[min(i+1, data.shape[0]-1), min(j+1, data.shape[0]-1)] != data[i, j]:
+                    if data[max(i - 1, 0), j] != data[i, j] or data[min(i + 1, data.shape[0] - 1), j] != data[i, j] or \
+                            data[i, max(j - 1, 0)] != data[i, j] or data[i, min(j + 1, data.shape[0] - 1)] != data[
+                        i, j] or data[max(i - 1, 0), max(j - 1, 0)] != data[i, j] or data[
+                        max(i - 1, 0), min(j + 1, data.shape[0] - 1)] != data[i, j] or data[
+                        min(i + 1, data.shape[0] - 1), max(j - 1, 0)] != data[i, j] or data[
+                        min(i + 1, data.shape[0] - 1), min(j + 1, data.shape[0] - 1)] != data[i, j]:
                         borders[i, j] = 1
         return borders
 
-    def color_image(connected_components_no_border, connected_components, border):
-        values = np.unique(connected_components_no_border)
-        colors = random.sample(range(2, 20), len(values)*2)
+    def color_image(connected_components_no_border, border_components):
+        values_no_border = np.unique(connected_components_no_border)[1:]
+        values_border = np.unique(border_components)[1:]
+        colors = random.sample(range(2, 20), len(values_no_border) + len(values_border))
         result = np.zeros(connected_components_no_border.shape, dtype=int)
-        for i, v in enumerate(values):
-            result[connected_components_no_border == v] = colors[i*2]
-            result[(connected_components == v) & (border == 1)] = colors[i * 2 + 1]
-        #result[border == 1] = 1
+
+        # color fields
+        for i in range(len(values_no_border)):
+            result[connected_components_no_border == values_no_border[i]] = colors[i]
+
+        # color borders
+        for i in range(len(values_border)):
+            result[border_components == values_border[i]] = colors[i + len(values_no_border)]
+
         return result
 
     def get_input(output, border):
@@ -89,7 +104,7 @@ def create_single_task_477d2879(image=None):
         for v in np.unique(output):
             index_v = np.where(output == v)
             if len(index_v[0]) > 1:
-                i = random.randint(0, len(index_v[0])-1)
+                i = random.randint(0, len(index_v[0]) - 1)
                 i1, i2 = index_v[0][i], index_v[1][i]
             else:
                 i1, i2 = index_v[0], index_v[1]
@@ -99,9 +114,12 @@ def create_single_task_477d2879(image=None):
 
     def check_if_ok(borders):
         color_bg = 1
-        for i in range(1, borders.shape[0]-1):
-            for j in range(1, borders.shape[1]-1):
-                if (borders[i-1, j] == color_bg or i-1 == 0) and (borders[i+1, j] == color_bg or i+1 == borders.shape[0]) and (borders[i, j-1] == color_bg or j-1==0) and (borders[i, j+1] == color_bg or j+1 == borders.shape[1]):
+        for i in range(1, borders.shape[0] - 1):
+            for j in range(1, borders.shape[1] - 1):
+                if (borders[i - 1, j] == color_bg or i - 1 == 0) and (
+                        borders[i + 1, j] == color_bg or i + 1 == borders.shape[0]) and (
+                        borders[i, j - 1] == color_bg or j - 1 == 0) and (
+                        borders[i, j + 1] == color_bg or j + 1 == borders.shape[1]):
                     return False
         return True
 
@@ -113,20 +131,18 @@ def create_single_task_477d2879(image=None):
         image[image == 255] = 0.
         image[image > 0] = 1.
 
+    # generate initial image with shapes and borders
     connected_components = remove_single_pixels(image)
     connected_components = remove_small_components(connected_components)
     connected_components = skimage.measure.label(connected_components, connectivity=2)
     borders = mark_border(connected_components)
 
-    # Hack to fix problem with components that share a border (remove border and relabel)
+    # extract connected components of shapes and borders
     connected_components_no_border = skimage.measure.label(1 - borders, connectivity=1)
-    # connected_components_no_border = connected_components.copy()
-    # connected_components_no_border[borders == 1] = 0
-    # connected_components_no_border = skimage.measure.label(connected_components_no_border, connectivity=2)
-    # connected_components_no_border[borders == 1] = 1
+    border_components = skimage.measure.label(borders, connectivity=1)
 
-    # connected_components_no_border is needed to get different colors for different borders
-    result = color_image(connected_components_no_border, connected_components, borders)
+    # generate input and output
+    result = color_image(connected_components_no_border, border_components)
     input_data = get_input(result, borders)
 
     if not check_if_ok(borders):
@@ -157,13 +173,18 @@ def create_single_task_477d2879(image=None):
     return task_dict
 
 
-
-def create_task_08ed6ac7(n_samples, path=Path('08ed6ac7_v2.json')):
-    assert not path.exists()
+def create_task(task_f, n_samples, path):
+    if path.exists():
+        answer = input(f"File {path} already exists. Overwrite? (y/n)")
+        if answer == "y":
+            path.unlink()
+        else:
+            warnings.warn(f"File {path} already exists. Not overwritten.")
+            return
 
     tasks = []
     while len(tasks) < n_samples:
-        task_dict = create_single_task_08ed6ac7()
+        task_dict = task_f()
         if task_dict is not None:
             tasks.append(task_dict)
     data = {"train": tasks}
@@ -172,19 +193,14 @@ def create_task_08ed6ac7(n_samples, path=Path('08ed6ac7_v2.json')):
         json.dump(data, fp)
 
 
+def create_task_08ed6ac7(n_samples, path=Path('08ed6ac7_v2.json')):
+    create_task(create_single_task_08ed6ac7, n_samples, path)
+
+
 def create_task_477d2879(n_samples, path=Path('477d2879_v2.json')):
-    assert not path.exists()
-
-    tasks = []
-    for _ in range(n_samples):
-        task_dict = create_single_task_477d2879()
-        tasks.append(task_dict)
-    data = {"train": tasks}
-
-    with open(path, 'w') as fp:
-        json.dump(data, fp)
+    create_task(create_single_task_477d2879, n_samples, path)
 
 
 if __name__ == '__main__':
-    # create_task_08ed6ac7(n_samples=args.n_samples)
+    create_task_08ed6ac7(n_samples=args.n_samples)
     create_task_477d2879(n_samples=args.n_samples)
