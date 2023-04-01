d019078c8c66051d678697e7c5fb3768f4147a72
Lukas Tuggener <tugg@zhaw.ch>
2023-04-01 20:58:12 +0200

    add init

--- alpaca/train.py
+++ alpaca/train.py
@@ -58,7 +58,7 @@ class TrainingArguments(transformers.TrainingArguments):
     cache_dir: Optional[str] = field(default=None)
     optim: str = field(default="adamw_torch")
     model_max_length: int = field(
-        default=450,
+        default=256,
         metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
     )
 
